16e0993ab83c7c432a8c29b7e9dfd5fa26111ecc
roxyross <rijjienterprise@gmail.com>
2024-03-20 12:15:48 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const digits = await inquirer.prompt([{
+        message: "Enter your First Digit :",
+        type: 'number',
+        name: "firstDigit",
+    },
+    {
+        message: "Enter your Second Digit :",
+        type: "number",
+        name: "secondDigit",
+    },
+    {
+        message: "Choose one of the operation to perform",
+        type: "list",
+        name: "operators",
+        choices: ["+", "-", "*", "/", "^", "%", "Reminder"],
+    },
+]);
+if (digits.operators === "+") {
+    console.log(`Your Value Is : ${digits.firstDigit} + ${digits.secondDigit} =`, digits.firstDigit + digits.secondDigit);
+}
+else if (digits.operators === "-") {
+    console.log(`Your Value Is : ${digits.firstDigit} - ${digits.secondDigit} =`, digits.firstDigit - digits.secondDigit);
+}
+else if (digits.operators === "*") {
+    console.log(`Your Value Is : ${digits.firstDigit} * ${digits.secondDigit} =`, digits.firstDigit * digits.secondDigit);
+}
+else if (digits.operators === "/") {
+    console.log(`Your Value Is : ${digits.firstDigit} / ${digits.secondDigit} =`, digits.firstDigit / digits.secondDigit);
+}
+else if (digits.operators === "^") {
+    console.log(`Your Value Is : ${digits.firstDigit} ^ ${digits.secondDigit} =`, digits.firstDigit ** digits.secondDigit);
+}
+else if (digits.operators === "%") {
+    console.log(`Your Value Is : ${digits.firstDigit} * ${digits.secondDigit} % =`, digits.firstDigit * digits.secondDigit / 100);
+}
+else if (digits.operators === "Reminder") {
+    console.log(`Your Value Is : ${digits.firstDigit} Reminder ${digits.secondDigit} =`, digits.firstDigit % digits.secondDigit);
+}
+else {
+    console.log("You have chooses a wrong operator.");
+}
